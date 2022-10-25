09dd7c03e02f064136b40bcdd586cda9d6f64185
CALUM SHEPHERD <2001125@uad.ac.uk>
2022-10-25 19:17:47 +0100

    Established connection to the database

--- ScottishGlen/ScottishGlen/Form1.cs
+++ ScottishGlen/ScottishGlen/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ScottishGlen
 {
@@ -15,6 +16,20 @@ namespace ScottishGlen
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine("Hello");
+
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=tolmount.abertay.ac.uk;Initial Catalog=mssql2001125;User ID=mssql2001125;Password=Jz7BTyghaF";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
