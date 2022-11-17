02feb32483ee33f37a1fd1361c2bd4be087e07b1
CALUM SHEPHERD <2001125@uad.ac.uk>
2022-11-17 11:42:37 +0000

    Added a new form for the main page with a logo and buttons to each other form

---
+++ ScottishGlen/ScottishGlen/AddForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ScottishGlen
+{
+    public partial class AddForm : Form
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+            dbConnect(); // runs this to immediately display the records on the progam loading
+        } // Form1()
+
+        void dbConnect() // function to connection to the database and display all records
+        {
+            string connetionString; // initialise connectionString to be valued with the details further down
+            SqlConnection cnn; // establish sql connection
+            connetionString = @"Data Source=tolmount.abertay.ac.uk;Initial Catalog=mssql2001125;User ID=mssql2001125;Password=Jz7BTyghaF"; // connection details to mssql database server
+            cnn = new SqlConnection(connetionString);
+            cnn.Open(); // open the db connection
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = ""; // initialise the sql and output strings
+
+            sql = "SELECT * FROM assets"; // sql statement to get all the records from the assets database
+            command = new SqlCommand(sql, cnn);
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read()) // while loop to loop through records in the database
+            {
+                // add each field from current record to output string
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2) + " - " + dataReader.GetValue(3) + " - " + dataReader.GetValue(4) + " - " + dataReader.GetValue(5) + " - " + dataReader.GetValue(6) + " - " + dataReader.GetValue(7) + "\n";
+                displayListData.Items.Add(Output); // add current Output to the list box to display
+                Output = ""; // clear Output so old Output isn't displayed again
+            } // while loop
+
+            cnn.Close(); // close connection
+        } // dbConnect()
+
+        void clearTextBoxes() // function to clear all the text boxes after an asset is added 
+        {
+            nameBox.Clear();
+            modelBox.Clear();
+            manufacturerBox.Clear();
+            typeBox.Clear();
+            ipBox.Clear();
+            purchDateBox.Clear();
+            noteBox.Clear();
+        } // clearTextBoxes()
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        } // Form1_load
+
+        private void addDataButton_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=tolmount.abertay.ac.uk;Initial Catalog=mssql2001125;User ID=mssql2001125;Password=Jz7BTyghaF"; // details to connect to the mssql db
+            cnn = new SqlConnection(connetionString); // initialise connection with the login details
+            cnn.Open(); // open connection 
+
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql="";
+
+            // inserting all the values from the text box to the database in an sql statement
+            sql = "INSERT INTO assets VALUES('" + nameBox.Text + "', '" + modelBox.Text + "', '" + manufacturerBox.Text + "', '" + typeBox.Text + "', '" + ipBox.Text + "', '" + purchDateBox.Text + "', '" + noteBox.Text + "'); ";
+            command = new SqlCommand(sql, cnn); // initialise the sql command
+
+            adapter.InsertCommand = new SqlCommand(sql, cnn); // start command
+            adapter.InsertCommand.ExecuteNonQuery(); // execute command
+
+            command.Dispose(); // dispose of the sql command
+            cnn.Close(); // close connection
+
+            clearTextBoxes();
+            dbConnect();
+        } // addDataButton_Click()
+    } // Form1 Class
+}
---
+++ ScottishGlen/ScottishGlen/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace ScottishGlen
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toAdd = new System.Windows.Forms.Button();
+            this.toView = new System.Windows.Forms.Button();
+            this.toEdit = new System.Windows.Forms.Button();
+            this.toDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(282, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 83);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toAdd
+            // 
+            this.toAdd.Location = new System.Drawing.Point(175, 176);
+            this.toAdd.Name = "toAdd";
+            this.toAdd.Size = new System.Drawing.Size(152, 44);
+            this.toAdd.TabIndex = 1;
+            this.toAdd.Text = "Add";
+            this.toAdd.UseVisualStyleBackColor = true;
+            this.toAdd.Click += new System.EventHandler(this.toAdd_Click);
+            // 
+            // toView
+            // 
+            this.toView.Location = new System.Drawing.Point(454, 176);
+            this.toView.Name = "toView";
+            this.toView.Size = new System.Drawing.Size(152, 44);
+            this.toView.TabIndex = 2;
+            this.toView.Text = "View";
+            this.toView.UseVisualStyleBackColor = true;
+            // 
+            // toEdit
+            // 
+            this.toEdit.Location = new System.Drawing.Point(175, 280);
+            this.toEdit.Name = "toEdit";
+            this.toEdit.Size = new System.Drawing.Size(152, 44);
+            this.toEdit.TabIndex = 3;
+            this.toEdit.Text = "Edit";
+            this.toEdit.UseVisualStyleBackColor = true;
+            // 
+            // toDelete
+            // 
+            this.toDelete.Location = new System.Drawing.Point(454, 280);
+            this.toDelete.Name = "toDelete";
+            this.toDelete.Size = new System.Drawing.Size(152, 44);
+            this.toDelete.TabIndex = 4;
+            this.toDelete.Text = "Delete";
+            this.toDelete.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toDelete);
+            this.Controls.Add(this.toEdit);
+            this.Controls.Add(this.toView);
+            this.Controls.Add(this.toAdd);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MainForm";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button toAdd;
+        private System.Windows.Forms.Button toView;
+        private System.Windows.Forms.Button toEdit;
+        private System.Windows.Forms.Button toDelete;
+    }
+}
