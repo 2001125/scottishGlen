262a77715070ee260f465198da7415a9639272a2
CALUM SHEPHERD <2001125@uad.ac.uk>
2022-11-24 12:57:06 +0000

    Created fully functioning login system with database, password still needs to be hashed/encrypted

--- ScottishGlen/ScottishGlen/LoginForm.cs
+++ ScottishGlen/ScottishGlen/LoginForm.cs
@@ -6,7 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Net; //Include this namespace
+using System.Management;
+
 
 namespace ScottishGlen
 {
@@ -33,18 +37,45 @@ namespace ScottishGlen
 
         private void submitButton_Click(object sender, EventArgs e)
         {
-            string username = "admin";
-            string password = "password";
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=tolmount.abertay.ac.uk;Initial Catalog=mssql2001125;User ID=mssql2001125;Password=Jz7BTyghaF"; // details to connect to the mssql db
+            cnn = new SqlConnection(connetionString); // initialise connection with the login details
+            cnn.Open(); // open connection
 
-            if (usernameTextBox.Text == username && passwordTextBox.Text == password)
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = ""; // initialise the sql and output strings
+
+            sql = "SELECT * FROM logins WHERE username = '" + usernameTextBox.Text + "';"; // sql statement to get the username
+            command = new SqlCommand(sql, cnn);
+            dataReader = command.ExecuteReader();
+
+            if (usernameTextBox.Text == "" || passwordTextBox.Text == "") // checks if the text boxes ar empty
             {
-                DialogResult = DialogResult.OK;
+                incorrectDetailsLabel.Text = "Please fill in all fields!"; // displays error message
+                incorrectDetailsLabel.Visible = true;
+            }
+            else if (dataReader.HasRows) // checks the username is in the database
+            {
+                while (dataReader.Read()) // loops through the records of the database
+                {
+                    if (passwordTextBox.Text == dataReader.GetValue(2).ToString()) // checks if the password is correct
+                    {
+                        DialogResult = DialogResult.OK; // loads the main form
+                    }
+                }
+            }
+            else
+            {
+                incorrectDetailsLabel.Text = "Username or password is incorrect!"; // displays error message if details are incorrect
+                incorrectDetailsLabel.Visible = true;
             }
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            Application.Exit(); //exits the program
         }
     }
 }
