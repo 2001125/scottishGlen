1d6de5ebeb61d912807e86e5663e6d79a49b49e2
CALUM SHEPHERD <2001125@uad.ac.uk>
2022-12-02 10:28:17 +0000

    added notes through comments

--- ScottishGlen/ScottishGlen/softwareForm.cs
+++ ScottishGlen/ScottishGlen/softwareForm.cs
@@ -16,5 +16,17 @@ namespace ScottishGlen
         {
             InitializeComponent();
         }
+
+        private void softwareForm_Load(object sender, EventArgs e)
+        {
+            // scan device to get the current os
+            // add it to the database
+            // when a certain os is clicked on, show user what hardware devices are currently using it
+            // probably just in one form to view, add, edit and delete
+            // for adding, don't allow manual add just scan system
+            // two tables (probably listboxes), one for the software and then when one is selected the other box shows the hardware using it
+            // have field in hardware database with foreign key showing what os it is using
+            string test; 
+        }
     }
 }
